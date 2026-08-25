9ff22009069a747d85d202290358592c46c11ee4
jazminkodland-glitch <jazminkodland@gmail.com>
2026-08-24 20:32:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
+            "jaz": "Nombre de persona"
             }
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
